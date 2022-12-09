a7fa2904941e0818957a67f33048f01c0f18f56e
Sascha Klomp <sascha.klomp@linkit.nl>
2022-12-09 12:18:42 +0100

    Refactor solution to make it a little more pretty

--- AdventOfCode/Day7.cs
+++ AdventOfCode/Day7.cs
@@ -28,35 +28,32 @@
                         var subDirectory = subInstructions[2];
                         currentObject = subDirectory switch
                         {
-                            ".."    => currentObject.Parent,
-                            "/"     => rootObject,
-                             _      => currentObject.SubObjects.First(s => s.Name.EndsWith(subDirectory))
-                         };
+                            ".." => currentObject.Parent,
+                            "/" => rootObject,
+                            _ => currentObject.SubObjects.First(s => s.Name.EndsWith(subDirectory))
+                        };
 
                     }
                 }
                 else if (isListing)
                 {
-                    
-                    if(subInstructions[0] == "dir")
+
+                    if (subInstructions[0] == "dir")
                     {
-                        currentObject.SubObjects.Add(CreateDirectory(subInstructions[1], currentObject));
+                        currentObject.SubObjects.Add(DirectoryObject.CreateDirectory(subInstructions[1], currentObject));
                     }
                     else if (int.TryParse(subInstructions[0], out int result))
                     {
-                        currentObject.SubObjects.Add(CreateFile(result, currentObject, subInstructions[1]));
+                        currentObject.SubObjects.Add(DirectoryObject.CreateFile(result, currentObject, subInstructions[1]));
                         AddSizeToDirectories(resultPartOneList, resultPartTwoList, currentObject, result);
                     }
                 }
-                
+
             });
 
             var answer1 = resultPartOneList.Sum(e => e.Size);
-            var TOTAL_SPACE = 70000000;
-            var freeSpace = TOTAL_SPACE - rootObject.Size;
-            var REQUIRED_SPACE = 30000000;
-            var spaceToFree = REQUIRED_SPACE - freeSpace;
-            var answer2 = resultPartTwoList.Where(x => x.Size >= spaceToFree).OrderBy(x => x.Size).ToList();
+            int spaceToFree = DirectoryObject.CalculateSpaceToFree(rootObject);
+            var answer2 = resultPartTwoList.Where(x => x.Size >= spaceToFree).OrderBy(x => x.Size).First().Size;
 
             Console.WriteLine($"{answer1} {answer2}");
 
@@ -79,8 +76,7 @@
                     AddOrUpdateList(resultListPartTwo, tempObject);
 
                 }
-
-                    
+   
                 tempObject = tempObject.Parent;
             }
         }
@@ -97,29 +93,6 @@
             resultList.Add(tempObject);
         }
 
-        private static DirectoryObject CreateFile(int result, DirectoryObject currentObject, string fileName)
-        {
-            return new DirectoryObject(fileName) { Parent = currentObject, Size = result };
-        }
 
-        private static DirectoryObject CreateDirectory(string directoryName, DirectoryObject currentObject)
-        {
-            var name = GenerateDirectoryName(currentObject, directoryName);
-            return new DirectoryObject(name) { Parent = currentObject, SubObjects = new List<DirectoryObject>() };
-        }
-
-        private static string GenerateDirectoryName(DirectoryObject currentObject, string directoryName)
-        {
-            var result = directoryName;
-            var tempObject = currentObject;
-            while(tempObject?.Name != "/")
-            {
-                result = $"{tempObject?.Name}/{result}";
-                tempObject = tempObject?.Parent;
-
-            }
-
-            return result;
-        }
     }
 }
---
+++ AdventOfCode/DirectoryObject.cs
@@ -0,0 +1,50 @@
+﻿namespace AdventOfCode
+{
+    internal class DirectoryObject
+    {
+        public DirectoryObject(string name)
+        {
+            Name = name;
+            SubObjects = new List<DirectoryObject>();
+        }
+
+        public int Size { get; set; }
+        public List<DirectoryObject> SubObjects { get; set; }
+        public string Name { get; set; }
+        public DirectoryObject? Parent { get; set; }
+
+        public static DirectoryObject CreateFile(int result, DirectoryObject currentObject, string fileName)
+        {
+            return new DirectoryObject(fileName) { Parent = currentObject, Size = result };
+        }
+
+        public static DirectoryObject CreateDirectory(string directoryName, DirectoryObject currentObject)
+        {
+            var name = GenerateDirectoryName(currentObject, directoryName);
+            return new DirectoryObject(name) { Parent = currentObject, SubObjects = new List<DirectoryObject>() };
+        }
+
+
+        public static int CalculateSpaceToFree(DirectoryObject rootObject)
+        {
+            var TOTAL_SPACE = 70000000;
+            var REQUIRED_SPACE = 30000000;
+
+            return REQUIRED_SPACE - (TOTAL_SPACE - rootObject.Size);
+        }
+        
+        private static string GenerateDirectoryName(DirectoryObject currentObject, string directoryName)
+        {
+            var result = directoryName;
+            var tempObject = currentObject;
+            while (tempObject?.Name != "/")
+            {
+                result = $"{tempObject?.Name}/{result}";
+                tempObject = tempObject?.Parent;
+
+            }
+
+            return result;
+        }
+    }
+}
